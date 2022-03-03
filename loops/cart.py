4f2407bf53d10c2bae3bba08735cd6e2eedafcf1
infinitedim <saputradimas.boediluhur@gmail.com>
2022-03-03 10:22:42 +0700

    adding for loops

---
+++ loops/cart.py
@@ -0,0 +1,7 @@
+import math
+prices = [10,20,30]
+
+total = 
+for price in prices:
+  total += price
+print(f"Total: {total}")
---
+++ loops/for.py
@@ -0,0 +1,17 @@
+'''
+# for loops using string
+for i in "Python":
+  print(i)
+'''
+
+'''
+# for loops using list 
+for item in [1,2,3,4,5]:
+  print(item)
+'''
+
+'''
+# for loops using range func
+for items in range(2, 11, 2):
+  print(items)
+'''
