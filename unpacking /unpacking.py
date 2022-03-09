f9303556a20bc21c241421fa2a9ab1fd1a277d14
infinitedim <saputradimas.boediluhur@gmail.com>
2022-03-09 18:07:54 +0700

    adding unpacking

---
+++ unpacking /unpacking.py
@@ -0,0 +1,17 @@
+# unpacking list
+
+list_coordinates = [1,2,3]
+
+# manual unpacking
+"""
+x = list_coordinates[0]
+y = list_coordinates[1]
+z = list_coordinates[2]
+print(x * y * z)
+"""
+
+# how to unpack automaticaly
+x y z = list_coordinates
+print(x * y * z)
+
+# this way can be use to unpack tuples
