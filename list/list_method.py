4e2fa87bd3de1f177633e5ec6b5d63351bfd62a2
infinitedim <saputradimas.boediluhur@gmail.com>
2022-03-09 17:49:19 +0700

    adding tuples

--- list/list_method.py
+++ list/list_method.py
@@ -2,8 +2,10 @@ numbers = [1,2,3,4,5,6,7,8,9,10]
 
 numbers.append(11) # to add new list index
 print(numbers)
-numbers.insert(7, 12) # to insert new list into particular index
+numbers.insert(7, 10) # to insert new list into particular index
 print(numbers) 
+print(numbers.count(10))# to find out how many numbers there are 10 in the list
+print(numbers.index(5)) # to find out index of number in the list
 numbers.sort() # to sorting list
 print(numbers)
 numbers.clear() # to clear list
---
+++ tuple/tuple.py
@@ -0,0 +1,14 @@
+numbers = (1,2,2,2,3,4)
+
+# tuple is a emutable data types
+# he can't be add, decrease, multiply, etc
+"""
+numbers.append() # will be error
+numbers.clear() # will be error
+numbers.insert() # will be error
+"""
+
+# Methods that can be used in tuples
+print(numbers.index(4))
+print(numbers.count(2))
+
