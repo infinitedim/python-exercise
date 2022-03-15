4a2bfa76f16f79d06e16c78495f536fd98a048aa
infinitedim <saputradimas.boediluhur@gmail.com>
2022-03-15 20:56:57 +0700

    adding class

---
+++ class/challange.py
@@ -0,0 +1,12 @@
+class Person:
+  def __init__(self, name):
+    self.name = name
+
+
+  def talk(self):
+    print(f"Hi, my name is {self.name}")
+     
+     
+dimas = Person("Dimas")
+
+print(Dimas.ta)
---
+++ class/class.py
@@ -0,0 +1,13 @@
+class Point:
+  def move(self):
+    print("move")
+   
+  def draw(self):
+    print("draw")
+    
+  
+point = Point()
+point.x = 10
+point.y = 20
+
+print(point.x)
